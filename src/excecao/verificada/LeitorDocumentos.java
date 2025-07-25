8b3eabc07f97972a9c9197f960fee5b0e06ccaab
Nelio Henrique <nelio_henrique@outlook.com>
2025-07-25 17:16:13 -0300

    testando git

--- src/excecao/verificada/LeitorDocumentos.java
+++ src/excecao/verificada/LeitorDocumentos.java
@@ -1,4 +1,12 @@
 package excecao.verificada;
 
 public class LeitorDocumentos {
+    public static void main(String[] args) {
+
+
+        var teste = "meu teste";
+        System.out.println( teste);
+
+
+    }
 }
